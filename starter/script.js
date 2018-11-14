8902a3694e5c39f71d5301a5843abedf39da56b5
kimbah <kim8hughes@iinet.net.au>
2018-11-14 10:29:14 +0800

    getWeather API with cors-anywhwhere (not crossorigin.me!)

--- starter/script.js
+++ starter/script.js
@@ -1,34 +1,34 @@
-const getIDs = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve([ 523, 883, 432, 974 ]);
-    }, 1500);
-});
+// const getIDs = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve([ 523, 883, 432, 974 ]);
+//     }, 1500);
+// });
 
-const getRecipe = (recID) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(
-            (ID) => {
-                const recipe = { title: 'Fresh tomato pasta', publisher: 'Jonas' };
-                resolve(`${ID}: ${recipe.title}`);
-            },
-            1500,
-            recID
-        );
-    });
-};
+// const getRecipe = (recID) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(
+//             (ID) => {
+//                 const recipe = { title: 'Fresh tomato pasta', publisher: 'Jonas' };
+//                 resolve(`${ID}: ${recipe.title}`);
+//             },
+//             1500,
+//             recID
+//         );
+//     });
+// };
 
-const getRelated = (publisher) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(
-            (pub) => {
-                const recipe = { title: 'Italian Pizza', publisher: 'Jonas' };
-                resolve(`${pub}: ${recipe.title}`);
-            },
-            1500,
-            publisher
-        );
-    });
-};
+// const getRelated = (publisher) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(
+//             (pub) => {
+//                 const recipe = { title: 'Italian Pizza', publisher: 'Jonas' };
+//                 resolve(`${pub}: ${recipe.title}`);
+//             },
+//             1500,
+//             publisher
+//         );
+//     });
+// };
 
 // getIDs
 //     .then((IDs) => {
@@ -46,14 +46,39 @@ const getRelated = (publisher) => {
 //         console.log('Error!!!');
 //     });
 
-async function getRecipesAW () {
-    const IDs = await getIDs;
-    console.log(IDs);
-    const recipe = await getRecipe(IDs[2]);
-    console.log(recipe);
-    const related = await getRelated('Jonas');
-    console.log(related);
+// async function getRecipesAW () {
+//     const IDs = await getIDs;
+//     console.log(IDs);
+//     const recipe = await getRecipe(IDs[2]);
+//     console.log(recipe);
+//     const related = await getRelated('Jonas');
+//     console.log(related);
 
-    return recipe;
+//     return recipe;
+// }
+// getRecipesAW().then((recipe) => console.log(`${recipe} is the best ever`));
+function getWeather (woeid) {
+    window
+        // .fetch(`https://crossorigin.me/https://www.metaweather.com/api/location/${woeid}/`)
+        .fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`)
+        .then((result) => {
+            // console.log(result);
+            return result.json();
+        })
+        .then((data) => {
+            // console.log(data);
+            const today = data.consolidated_weather[0];
+            console.log(`temperature in ${data.title} stay between ${today.min_temp} and ${today.max_temp}`);
+        })
+        .catch((error) => console.log(error));
 }
-getRecipesAW().then((recipe) => console.log(`${recipe} is the best ever`));
+getWeather(2487956);
+getWeather(44418);
+
+async function getWeatherAW (woeid) {
+    const result = await window.fetch(
+        `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`
+    );
+}
+getWeatherAW(2487956);
+getWeatherAW(44418);
